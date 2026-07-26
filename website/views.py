6515ceed9c961611d01851e874929c74230ad86b
Khenymosur <bellookehinde@gmail.com>
2026-07-26 01:53:04 +0100

    home-view

--- website/views.py
+++ website/views.py
@@ -1,9 +1,23 @@
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
 
 
 class HomeView(TemplateView):
 
     template_name = "website/home.html"
+
+    def dispatch(self, request, *args, **kwargs):
+
+        if request.user.is_authenticated:
+
+            # Staff and superusers
+            if request.user.is_staff or request.user.is_superuser:
+                return redirect("dashboard:index")   # Change if your URL name differs
+
+            # Customer
+            return redirect("customer_portal:dashboard")
+
+        return super().dispatch(request, *args, **kwargs)
 
 
 class AboutView(TemplateView):
